9208aefd81872b029435fd47af881d0a88f0d645
Kayes Islam <kayes.islam@spacetalk.co>
2023-07-05 23:10:09 +0930

    Removed the size constraints in an attempt to resize the window. Added smaller width dimention for the Linux app.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_multi_grid_reorderables/flutter_multi_grid_reorderables.dart';
 import 'package:flutter_multi_grid_reorderables/group.dart';
 
-
 void main() {
-  runApp(SizedBox.fromSize(size: const Size(100, 1000), child: const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,35 +17,18 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: FlutterMultiGridReorderable<String, String>(
-        data: [ Group<String, String>(
-            "Group A", 
-            [
-              "1",
-              "2",
-              "3",
-              "4",
-              "5"
-            ]
-          ),
-          Group<String, String>(
-            "Gorup B", 
-            [
-              "6",
-              "7",
-              "8",
-              "9",
-              "10"
-            ]
-          ),
+        data: [
+          Group<String, String>("Group A", ["1", "2", "3", "4", "5"]),
+          Group<String, String>("Gorup B", ["6", "7", "8", "9", "10"]),
         ],
         groupTitleBuilder: (groupData) => Text(groupData),
         groupButtonBuilder: (groupData) => TextButton(
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                ),
-                onPressed: () { },
-                child: const Icon(Icons.add),
-              ),
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+          ),
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
         blockBuilder: (str) => Text(str),
       ),
     );
